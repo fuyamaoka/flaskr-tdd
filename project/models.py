0a856a22ce0409ab5d5d2b9d3a0d26a95b82868d
Fu Yamaoka <fuyamaoka@gmail.com>
2024-10-15 13:46:16 -0400

    using SQLAlchemy

---
+++ project/models.py
@@ -0,0 +1,13 @@
+from project.app import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.String(500), nullable=False)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
